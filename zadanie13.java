c61c868c702b0c4ddf0c7adfa6a688c556eb60ef
MeDyzS211 <artem_alekhin_2000@mail.ru>
2025-12-24 20:06:07 +0300

    вариант 1 задание 13

---
+++ zadanie13.java
@@ -0,0 +1,11 @@
+public class zadanie13 {
+    public static void main(String[] args) {
+        int[] b = {-2, 3, 5, -1};
+
+        for (int i = 0; i < b.length; i++) {
+            if (b[i] > 0) {
+                System.out.print(b[i] * 5 + " ");
+            }
+        }
+    }
+}
