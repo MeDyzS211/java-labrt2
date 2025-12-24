f6ceb7dfc332a8a0e49108c1b1cc1306fa15c568
MeDyzS211 <artem_alekhin_2000@mail.ru>
2025-12-24 19:56:39 +0300

    вариант 1 задание 1

---
+++ zadanie1.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class Task1 {
+    public static final int SIZE = 10;
+    public static final int VALUE = 5;
+
+    public static void main(String[] args) {
+        int[] a = new int[SIZE];
+        Random r = new Random();
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = r.nextInt(199) - 99;
+            System.out.print(a[i] + " ");
+        }
+
+        System.out.println("\nНомера элементов равных " + VALUE + ":");
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == VALUE) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
