b8ae9a23855f7f970133dd446b19cda047ad1ce2
MeDyzS211 <artem_alekhin_2000@mail.ru>
2025-12-24 19:57:14 +0300

    вариант 1 задание 3

---
+++ zadanie3.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Task3 {
+    public static final int VALUE = 0;
+
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        Random r = new Random();
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = r.nextInt(199) - 99;
+            System.out.print(a[i] + " ");
+        }
+
+        System.out.println("\nЭлементы меньше " + VALUE + ":");
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < VALUE) {
+                System.out.println("a[" + i + "] = " + a[i]);
+            }
+        }
+    }
+}
